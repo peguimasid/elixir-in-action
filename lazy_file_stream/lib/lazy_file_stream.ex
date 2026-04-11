c390a91aa437da12103ce4a1c54d513da10d99af
Guilhermo Masid <guilhermomasid@gmail.com>
2026-04-11 19:42:20 -0300

    add some more

--- lazy_file_stream/lib/lazy_file_stream.ex
+++ lazy_file_stream/lib/lazy_file_stream.ex
@@ -11,4 +11,17 @@ defmodule LazyFileStream do
     |> Stream.map(&String.trim_trailing(&1, "\n"))
     |> Enum.map(&String.length/1)
   end
+
+  def longest_line_length!(path) do
+    path
+    |> lines_lengths!()
+    |> Enum.max()
+  end
+
+  def longest_line!(path) do
+    path
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing(&1, "\n"))
+    |> Enum.max_by(&String.length/1)
+  end
 end
