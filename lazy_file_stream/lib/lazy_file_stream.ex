9f1349289b3756a1bf6988d18500f90f2b26f7c7
Guilhermo Masid <guilhermomasid@gmail.com>
2026-04-11 19:34:42 -0300

    add lines lengths

--- lazy_file_stream/lib/lazy_file_stream.ex
+++ lazy_file_stream/lib/lazy_file_stream.ex
@@ -12,4 +12,11 @@ defmodule LazyFileStream do
     |> Stream.map(&String.trim_trailing(&1, "\n"))
     |> Enum.filter(&(String.length(&1) > 80))
   end
+
+  def lines_lengths!(path) do
+    path
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing(&1, "\n"))
+    |> Enum.map(&String.length/1)
+  end
 end
