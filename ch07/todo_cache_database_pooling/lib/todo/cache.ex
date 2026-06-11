449e07f6cf1cde78cdee57739cb4925811ff743a
Guilhermo Masid <guilhermomasid@gmail.com>
2026-06-11 19:17:20 -0300

    add folder to start db pooling

---
+++ ch07/todo_cache_database_pooling/lib/todo/cache.ex
@@ -0,0 +1,33 @@
+defmodule Todo.Cache do
+  use GenServer
+
+  # public api
+  def start do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def server_process(cache_pid, todo_list_name) do
+    GenServer.call(cache_pid, {:server_process, todo_list_name})
+  end
+
+  # genserver callbacks
+  @impl GenServer
+  def init(_) do
+    Todo.Database.start()
+    {:ok, %{}}
+  end
+
+  @impl GenServer
+  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
+    case Map.fetch(todo_servers, todo_list_name) do
+      {:ok, todo_server} ->
+        {:reply, todo_server, todo_servers}
+
+      :error ->
+        {:ok, new_server} = Todo.Server.start(todo_list_name)
+        todo_servers = Map.put(todo_servers, todo_list_name, new_server)
+
+        {:reply, new_server, todo_servers}
+    end
+  end
+end
