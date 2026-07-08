99a55b7c036b257039221b568e11533499babc66
Guilhermo Masid <guilhermomasid@gmail.com>
2026-07-08 19:37:40 -0300

    its working so far

---
+++ ch10/process_registry/gen_server.ex
@@ -0,0 +1,40 @@
+defmodule SimpleRegistry do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def register(key) do
+    GenServer.call(__MODULE__, {:register, key})
+  end
+
+  def whereis(key) do
+    GenServer.call(__MODULE__, {:whereis, key})
+  end
+
+  # Server calls
+
+  @impl true
+  def init(_) do
+    Process.flag(:trap_exit, true)
+    {:ok, %{}}
+  end
+
+  @impl true
+  def handle_call({:register, key}, {caller_pid, _tag}, process_registry) do
+    case Map.get(process_registry, key) do
+      nil ->
+        Process.link(caller_pid)
+        {:reply, :ok, Map.put(process_registry, key, caller_pid)}
+
+      _ ->
+        {:reply, :error, process_registry}
+    end
+  end
+
+  @impl true
+  def handle_call({:whereis, key}, _from, process_registry) do
+    {:reply, Map.get(process_registry, key), process_registry}
+  end
+end
--- ch10/process_registry/test/process_registry_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ProcessRegistryTest do
-  use ExUnit.Case
-  doctest ProcessRegistry
-
-  test "greets the world" do
-    assert ProcessRegistry.hello() == :world
-  end
-end
